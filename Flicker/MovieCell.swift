328ebc4d0e2fae5b92c2eb7798be26d5181b9ab9
Michael J Hammond <hammondmj@yahoo.com>
2016-02-02 00:57:06 -0500

    Week 1 work complete

---
+++ Flicker/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicker
+//
+//  Created by Melos on 2/2/16.
+//  Copyright © 2016 Melos. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
