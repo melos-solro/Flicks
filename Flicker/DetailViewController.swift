d82921dfcbc6f227307ec4b8419c517580887022
Melos <Melos@meloss-mbp.home>
2016-02-08 23:53:59 -0500

    Completion of detail view, week 2 pre work

---
+++ Flicker/DetailViewController.swift
@@ -0,0 +1,73 @@
+//
+//  DetailViewController.swift
+//  Flicker
+//
+//  Created by Melos on 2/8/16.
+//  Copyright © 2016 Melos. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class DetailViewController: UIViewController {
+
+ 
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var infoView: UIView!
+    var movie: NSDictionary!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: (infoView.frame.origin.y + infoView.frame.size.height))
+        
+        let title = movie["title"] as! String
+        titleLabel.text = title
+        
+        let overview = movie["overview"] as! String
+        overviewLabel.text = overview
+        
+        overviewLabel.sizeToFit()
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let posterUrl = NSURL(string: posterBaseUrl + posterPath)
+            posterImageView.setImageWithURL(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            posterImageView.image = nil
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+
+        
+        
+    }
+    */
+
+}
